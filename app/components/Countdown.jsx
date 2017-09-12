0d90bfd1792289fbd7df1cca1a6047d0b638d39d
nthigamac <dmdmasha28@gmail.com>
2017-09-12 15:12:40 +0300

    Add navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,14 @@
+ var React = require('react');
+
+var Countdown = React.createClass({
+  render: function(){
+    return(
+      <div>
+        <h2> Countdown Component</h2>
+      </div>
+    )
+  }
+
+});
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var Timer = React.createClass({
+  render: function(){
+    return(
+      <div>
+        <h2> This is the Timer </h2>
+      </div>
+    )
+  }
+
+});
+
+module.exports = Timer;
